8edb4924fe705671176d7a7c480c8dd7848f9912
Aleksandra Galach <rachelkakoderka@gmail.com>
2022-09-06 21:57:17 +0200

    All elements on page, now only styling

---
+++ src/Buttons.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Buttons() {
+    return (
+        <div id="App-buttons">
+            <button type="button" id="mail-btn">Email</button>
+            <button type="button" id="lin-btn">LinkedIn</button>
+        </div>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+    return (
+			<div id="App-header">
+				<img
+					src="https://images.unsplash.com/photo-1611915387288-fd8d2f5f928b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+					alt="cat"
+					width="317px"
+				/>
+				<h1>Laura Smith</h1>
+				<h3>Frontend Developer</h3>
+				<h4>laurasmith.website</h4>
+			</div>
+		);
+}
